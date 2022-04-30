27a4fcf1b9ae5040c85d92469d28ac0c6d4a5554
Syargik <syargik@gmail.com>
2022-04-30 21:47:20 +0300

    refactor(refactored topba): refactoring of topar.js code

--- js/topbar.js
+++ js/topbar.js
@@ -1,100 +1,135 @@
 class TopBar {
     constructor() {
-        this.profile = document.querySelector(`.profile__image`);
-        this.online = document.querySelector(`.online__status`);
-        this.mode = document.querySelector(`.color__mod`);
-        this.language = document.querySelector(`.language`);
-        this.languageBack = document.querySelector(`.languages__top`);
-        this.languages = document.querySelector(`.languages`);
-        this.menu = document.querySelector(`.profile__menu`);
-        this.authMenu = document.querySelector(`.authorize__menu`);
-        this.tabs = document.querySelectorAll(`.tab`);
-        this.authButtons = document.querySelectorAll(`.js-showAuthMenu`);
-        this.body = document.body;
+        this.elements = {}
         this.init();
     }
 
-    dropdownMenu() {
-        this.body.addEventListener(`click`, ({ target }) => {
-            if (!target.closest(`.dropdown`) && !target.closest(`.profile`)) {
-                document.querySelectorAll(`.dropdown`).forEach((element) => element.classList.add(`hidden`))
-            }
-        });
-    }
-
-    profileMenu() {
-        if (this.profile) {
-            this.profile.addEventListener(`click`, () => {
-                this.menu.classList.toggle(`hidden`);
-            })
-            this.online.addEventListener(`click`, () => {
-                document.querySelector(`.menu__user`).classList.toggle(`online`);
-                document.querySelector(`.profile`).classList.toggle(`online`);
-                document.getElementById(`online`).classList.toggle(`active`);
-            })
-
-            this.mode.addEventListener(`click`, () => {
-                document.getElementById(`mode`).classList.toggle(`active`);
-            })
-
-            this.language.addEventListener(`click`, () => {
-                const languageMenu = document.querySelector(`.language__menu`);
-                languageMenu.classList.remove(`hidden`);
-                this.menu.classList.add(`hidden`);
-            })
-
-            this.languageBack.addEventListener(`click`, () => {
-                const languageMenu = document.querySelector(`.language__menu`);
-                languageMenu.classList.add(`hidden`);
-                this.menu.classList.remove(`hidden`);
-            })
-
-            this.languages.addEventListener(`click`, ({ target }) => {
-                const language = target.closest(`label`)
-                document.querySelectorAll(`.languages label`).forEach((element) => element.classList.remove(`active`))
-                language.classList.add(`active`)
-            })
+    dropdownMenu(target) {
+        if (!target.closest(`.dropdown`) && !target.closest(`.profile`)) {
+            document.querySelectorAll(`.dropdown`).forEach((element) => element.classList.add(`hidden`))
         }
     }
 
+    showProfileMenu() {
+        this.elements.menu.classList.toggle(`hidden`);
+    }
+
+    onlineChanging() {
+        document.querySelector(`.menu__user`).classList.toggle(`online`);
+        document.querySelector(`.profile`).classList.toggle(`online`);
+        document.getElementById(`online`).classList.toggle(`active`);
+    }
+
+    modeChanging() {
+        document.getElementById(`mode`).classList.toggle(`active`);
+    }
+
+    showLanguageMenu() {
+        this.elements.languageMenu.classList.remove(`hidden`);
+        this.elements.menu.classList.add(`hidden`);
+    }
+
+    backLanguageMenu() {
+        this.elements.languageMenu.classList.add(`hidden`);
+        this.elements.menu.classList.remove(`hidden`);
+    }
+
+    languageChoose(target) {
+        const language = target.closest(`label`)
+        document.querySelectorAll(`.languages label`).forEach((element) => element.classList.remove(`active`))
+        language.classList.add(`active`)
+    }
+
+    tabsFunc(target) {
+        this.clearTabs();
+        document.querySelector(`#${target.id}`).classList.add(`active`)
+        document.querySelector(`[data-tab-id=${target.id}]`).classList.remove(`hidden`)
+    }
+
+    unAuthorizedButtons(target) {
+        this.clearTabs();
+        this.elements.modal.show();
+        document.querySelector(`.authorize__menu`).classList.remove(`hidden`)
+        document.querySelector(`#auth${target.id}`).classList.add(`active`)
+        document.querySelector(`[data-tab-id=auth${target.id}]`).classList.remove(`hidden`)
+    }
+
     clearTabs() {
-        this.tabs.forEach((element) => element.classList.remove(`active`))
+        this.elements.tabs.forEach((element) => element.classList.remove(`active`))
         document.querySelectorAll(`[data-tab-id]`).forEach((element) => element.classList.add(`hidden`))
     }
 
-    tabsFunc() {
-        this.tabs.forEach(element => {
-            element.addEventListener(`click`, ({ target }) => {
-                this.clearTabs();
-                document.querySelector(`#${target.id}`).classList.add(`active`)
-                document.querySelector(`[data-tab-id=${target.id}]`).classList.remove(`hidden`)
-            })
-        });
-    }
-
-    modalMenu() {
-        const modal = new Modal(`auth`, {
+    bindElements() {
+        this.elements.profile = document.querySelector(`.profile__image`);
+        this.elements.online = document.querySelector(`.online__status`);
+        this.elements.mode = document.querySelector(`.color__mod`);
+        this.elements.language = document.querySelector(`.language`);
+        this.elements.languageBack = document.querySelector(`.languages__top`);
+        this.elements.languages = document.querySelector(`.languages`);
+        this.elements.menu = document.querySelector(`.profile__menu`);
+        this.elements.authMenu = document.querySelector(`.authorize__menu`);
+        this.elements.tabs = document.querySelectorAll(`.tab`);
+        this.elements.authButtons = document.querySelectorAll(`.js-showAuthMenu`);
+        this.elements.body = document.body;
+        this.elements.languageMenu = document.querySelector(`.language__menu`);
+        this.elements.modal = new Modal(`auth`, {
             background: true,
             closeOnBg: true,
             showDuration: `0`
         });
+    }
 
-        this.authButtons.forEach(element => {
+    bindHandlers() {
+        this.elements.body.addEventListener(`click`, ({ target }) => {
+            this.dropdownMenu(target);
+        });
+
+        if (this.elements.profile) {
+
+            this.elements.profile.addEventListener(`click`, () => {
+                this.showProfileMenu();
+            })
+
+            this.elements.online.addEventListener(`click`, () => {
+                this.onlineChanging();
+            })
+
+            this.elements.mode.addEventListener(`click`, () => {
+                this.modeChanging();
+            })
+
+            this.elements.language.addEventListener(`click`, () => {
+                this.showLanguageMenu();
+            })
+
+            this.elements.languageBack.addEventListener(`click`, () => {
+                this.backLanguageMenu();
+            })
+
+            this.elements.languages.addEventListener(`click`, ({ target }) => {
+                this.languageChoose(target);
+            })
+
+        }
+
+        this.elements.tabs.forEach(element => {
             element.addEventListener(`click`, ({ target }) => {
-                this.clearTabs();
-                modal.show();
-                document.querySelector(`.authorize__menu`).classList.remove(`hidden`)
-                document.querySelector(`#auth${target.id}`).classList.add(`active`)
-                document.querySelector(`[data-tab-id=auth${target.id}]`).classList.remove(`hidden`)
+                this.tabsFunc(target);
+            })
+        });
+
+        this.elements.authButtons.forEach(element => {
+            element.addEventListener(`click`, ({ target }) => {
+                this.unAuthorizedButtons(target);
             })
         })
     }
 
     init() {
-        this.dropdownMenu();
-        this.profileMenu();
+        this.bindElements();
+        this.bindHandlers();
         this.clearTabs();
-        this.tabsFunc();
-        this.modalMenu();
     }
 }
+
+new TopBar()
